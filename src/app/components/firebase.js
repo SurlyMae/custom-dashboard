f7ee9521f4ab0613852472bddbcd42a0849a90a5
SurlyMae <surlymae@gmail.com>
2016-11-29 11:56:32 -0700

    feat(login): Add Firebase login, signup, logout

--- src/app/components/firebase.js
+++ src/app/components/firebase.js
@@ -13,3 +13,40 @@ const dbRef = firebase.database().ref();
 dbRef.on('value', function(snap) {
   console.log(snap.val())
 });
+
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogout = document.getElementById('btnLogout');
+
+btnLogin.addEventListener('click', e => {
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  const promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+});
+
+btnSignUp.addEventListener('click', e => {
+  // TODO: check for real email
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  const promise = auth.createUserWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+});
+
+btnLogout.addEventListener('click', e => {
+  firebase.auth().signOut();
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    console.log(firebaseUser);
+    btnLogout.classList.remove('hide');
+  } else {
+    console.log('not logged in');
+    btnLogout.classList.add('hide');
+  }
+});
