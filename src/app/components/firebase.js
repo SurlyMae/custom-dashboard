784c7b335f425a7c7d14f441d87ffb687c69a652
SurlyMae <surlymae@gmail.com>
2016-11-30 10:20:14 -0700

    feat(customer search): Add database query with on-click

--- src/app/components/firebase.js
+++ src/app/components/firebase.js
@@ -11,7 +11,7 @@ firebase.initializeApp(config);
 const dbRef = firebase.database().ref();
 
 dbRef.on('value', function(snap) {
-  console.log(snap.val())
+  // console.log("all data is ", snap.val());
 });
 
 const txtEmail = document.getElementById('txtEmail');
@@ -43,20 +43,44 @@ btnLogout.addEventListener('click', e => {
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser) {
-    console.log(firebaseUser);
+    // console.log(firebaseUser);
     btnLogout.classList.remove('hide');
   } else {
-    console.log('not logged in');
+    // console.log('not logged in');
     btnLogout.classList.add('hide');
   }
 });
 
 function getName () {
   let name = document.getElementById('nameSearch').value;
-  console.log(name);;
-}
+  console.log(name);
+  nameQuery(name);
+};
 
 function getNumber () {
   let number = document.getElementById('numberSearch').value;
-  console.log(number);;
+  console.log(number);
+  numberQuery(number);
 }
+
+function nameQuery (name) {
+  console.log("nameQuery func has name", name);
+
+  const query = dbRef.orderByChild('ClientName')
+  .equalTo(name).limitToFirst(10);
+
+  query.on("value", function(snapshot) {
+    console.log("sorted data is ", snapshot.val());
+  });
+};
+
+function numberQuery (number) {
+  console.log("numberQuery func has number", number);
+
+  const query = dbRef.orderByChild('ClientOrder')
+  .equalTo(number).limitToFirst(10);
+
+  query.on("value", function(snapshot) {
+    console.log("sorted data is ", snapshot.val());
+  });
+};
