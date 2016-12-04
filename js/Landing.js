b8943b5a92188d5ad6e5e188791b718c11674216
SurlyMae <surlymae@gmail.com>
2016-12-04 10:24:21 -0700

    WIP(React): Add first components to app, move others

---
+++ js/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = React.createClass({
+  render () {
+    return (
+      <div className='landing'>
+        <h1>Landing Page</h1>
+      </div>
+    )
+  }
+})
+
+export default Landing
