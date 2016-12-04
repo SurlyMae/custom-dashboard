5df9af2ee4188f05e0444ee5d699e3a5143e645a
SurlyMae <surlymae@gmail.com>
2016-12-04 15:01:43 -0700

    WIP(React): Work on search logic

--- js/Search.js
+++ js/Search.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import preload from '../public/orders.json'
+import ShowOrder from './ShowOrder'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
-      <h1>Search here</h1>
+      <div className='search'>
+        <h1>Search here</h1>
+        <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placedholder='Enter order number' />
+        {preload.orders.filter((order) => {
+          return `${order.ClientName} ${order.ClientOrder}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+        })
+        .map((order) => {
+          return (
+            <ShowOrder key={order.ISupplyOrder} {...order} />
+          )
+        })}
+      </div>
     )
   }
 })
